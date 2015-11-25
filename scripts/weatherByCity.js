eb803ce24127d6001e15c1665ad0466e4d8a7b52
koldovsky <koldovsky@yahoo.com>
2015-11-25 08:09:49 +0000

    Added API key

--- scripts/weatherByCity.js
+++ scripts/weatherByCity.js
@@ -1,7 +1,7 @@
 function getWeatherByCity(lang, fnOK, fnError, cityName) {
     $.getJSON(
         'http://api.openweathermap.org/data/2.5/forecast/daily?q=' 
-        + cityName + '&cnt=16&units=metric' + '&lang=' + lang + '&callback=?',
+        + cityName + '&cnt=16&units=metric' + '&lang=' + lang + '&callback=?&APPID=85dd9ac97b8b9d0cbc5f814a5b96ffa6',
         function (data) {
             fnOK.call(this, data);
         }
